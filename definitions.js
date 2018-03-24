86ec096d655aa5d83cbd0f06da83d5670c45a427
graemeerickson <graemeerickson@gmail.com>
2018-03-24 16:11:38 -0700

    Added some method definitions

--- definitions.js
+++ definitions.js
@@ -5,21 +5,42 @@ const card = [
       objectType: "array",
       purpose: "Change the contents of an array by removing existing elements and/or adding new elements.",
       parameters: "sample parameter",
-      impactsObject: "yes"
+      impactsObject: "Yes"
    },
    {
       methodName: "splice()",
       objectType: "array",
       purpose: "Add or remove items from an array.",
       parameters: "3 - (where new element should be added, number of elements to remove, new element to add)",
-      impactsObject: "yes"
+      impactsObject: "Yes"
    },
    {  
       methodName: "slice()",
       objectType: "array",
       purpose: "Get some elements of an array.",
       parameters: "2 - (starting element, number of elements to get)",
-      impactsObject: "no"
+      impactsObject: "No"
+   },
+   {
+      methodName: "push()",
+      objectType: "array",
+      purpose: "Add a new element to the end of an array.",
+      parameters: "1 - (element to add)",
+      impactsObject: "No"
+   },
+   {
+      methodName: "pop()",
+      objectType: "array",
+      purpose: "Remove the last element from an array.",
+      parameters: "None",
+      impactsObject: "No"
+   },
+   {
+      methodName: "toString()",
+      objectType: "array",
+      purpose: "Converts an array to a string of comma-separated values.",
+      parameters: "None",
+      impactsObject: "No"
    },
    // array functions
    {  
@@ -27,21 +48,21 @@ const card = [
       objectType: "array",
       purpose: "sample purpose",
       parameters: "sample parameter",
-      impactsObject: "tbd"
+      impactsObject: "Tbd"
    },
    {  
       methodName: "reduce()",
       objectType: "array",
       purpose: "sample purpose",
       parameters: "sample parameter",
-      impactsObject: "tbd"
+      impactsObject: "Tbd"
    },
    {
       methodName: "map()",
       objectType: "array",
       purpose: "sample purpose",
       parameters: "sample parameter",
-      impactsObject: "no"
+      impactsObject: "No"
    }
    // string methods
 
