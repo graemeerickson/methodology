32b2508631577f43e93d7c4fdf783de98f3d5d0a
graemeerickson <graemeerickson@gmail.com>
2018-03-24 17:14:35 -0700

    Added method definitions

--- definitions.js
+++ definitions.js
@@ -2,136 +2,136 @@ const card = [
    // array methods
    {
       methodName: "toString()",
-      objectType: "array",
+      objectType: "Array",
       purpose: "Convert an array to a new string of comma-separated values.",
       parameters: "None",
       impactsObject: "No"
    },
    {
       methodName: "join()",
-      objectType: "array",
+      objectType: "Array",
       purpose: "Join all array elements into a new string and insert separator between them if desired.",
-      parameters: "1 - (separator)",
+      parameters: "(1) separator",
       impactsObject: "No"
    },
    {
       methodName: "push()",
-      objectType: "array",
+      objectType: "Array",
       purpose: "Add a new element to the end of an array.",
-      parameters: "1 - (element to add to the array)",
+      parameters: "(1) element to add to the array",
       impactsObject: "Yes"
    },
    {
       methodName: "pop()",
-      objectType: "array",
+      objectType: "Array",
       purpose: "Remove the last element from an array.",
       parameters: "None",
       impactsObject: "Yes"
    },
    {
       methodName: "shift()",
-      objectType: "array",
+      objectType: "Array",
       purpose: "Remove the first array element and shifts all other elements to a lower index",
       parameters: "None",
       impactsObject: "Yes"
    },
    {
       methodName: "unshift()",
-      objectType: "array",
+      objectType: "Array",
       purpose: "Add a new element to the beginning of an array, and unshift older elements (i.e., shift them to a higher index).",
       parameters: "None",
       impactsObject: "Yes"
    },
    {
       methodName: "splice()",
-      objectType: "array",
+      objectType: "Array",
       purpose: "Add or remove items from an array.",
-      parameters: "3 - (where new element should be added, number of elements to remove, new element to add)",
+      parameters: "(1) where new element should be added, (2) number of elements to remove, (3) new element to add)",
       impactsObject: "Yes"
    },
    {
       methodName: "concat()",
-      objectType: "array",
+      objectType: "Array",
       purpose: "Create a new array by merging (concatenating) existing arrays.",
-      parameters: "Any - (array1, array2, array3, ...",
+      parameters: "Any - (array1, array2, array3, ...)",
       impactsObject: "No"
    },
    {  
       methodName: "slice()",
-      objectType: "array",
+      objectType: "Array",
       purpose: "Slice some elements from an array into a new array.",
-      parameters: "2 - (starting element, number of elements to get)",
+      parameters: "(1) starting element, (2) number of elements to get",
       impactsObject: "No"
    },
    {
       methodName: "sort()",
-      objectType: "array",
+      objectType: "Array",
       purpose: "Sort an array alphabetically. Remember that it sorts as strings; use a compare function to sort numerically: array.sort(function(a, b) { return a - b });",
       parameters: "None",
       impactsObject: "Yes"
    },
    {
       methodName: "reverse()",
-      objectType: "array",
-      purpose: "Sort an array in descending order. Remember that it sorts as strings; use a compare function to sort numerically: array.sort(function(a, b) { return b - a });",
+      objectType: "Array",
+      purpose: "Sort an array in descending order. Remember that it sorts as strings; use a compare function to sort numerically: array.sort(function(a, b) { return b - a });.",
       parameters: "None",
       impactsObject: "Yes"
    },
    {
       methodName: "indexOf()",
-      objectType: "array",
+      objectType: "Array",
       purpose: "Search an array for an item and returns its position. If found more than once, return its first position. Note: returns -1 if item is not found.",
-      parameters: "2 - (item to search for, starting point in array)",
+      parameters: "(1) item to search for, (2) starting point in array (optional)",
       impactsObject: "No"
    },
    {
       methodName: "lastIndexOf()",
-      objectType: "array",
+      objectType: "Array",
       purpose: "Search an array for an item and returns its position. If found more than once, return its last position. Note: returns -1 if item is not found.",
-      parameters: "2 - (item to search for, starting point in array)",
+      parameters: "(1) item to search for, (2) starting point in array (optional)",
       impactsObject: "No"
    },
    {
       methodName: "isArray()",
-      objectType: "array",
+      objectType: "Array",
       purpose: "Check whether than object is an array.",
-      parameters: "1 - (object name)",
+      parameters: "(1) object name",
       impactsObject: "No"
    },
    // array functions
    {  
       methodName: "filter()",
-      objectType: "array",
+      objectType: "Array",
       purpose: "Create a new array with every element in an array that passes a test/condition.",
-      parameters: "3 - (Value of the current element, array index of the current element, array object the current element belongs to)",
+      parameters: "(1) value of the current element, (2) array index of the current element (optional), (3) array object the current element belongs to (optional)",
       impactsObject: "No"
    },
    {  
       methodName: "reduce()",
-      objectType: "array",
+      objectType: "Array",
       purpose: "Reduce (e.g., sum) the values of an array to a single value (going left-to-right)",
-      parameters: "4 - (total - initialValue or previously returned value of the function, Value of the current element, array index of the current element, array object the current element belongs to)",
+      parameters: "(1) total - initialValue or previously returned value of the function, (2) value of the current element, (3) array index of the current element (optional), (4) array object the current element belongs to (optional)",
       impactsObject: "No"
    },
    {  
       methodName: "reduceRight()",
-      objectType: "array",
+      objectType: "Array",
       purpose: "Reduce (e.g., sum) the values of an array to a single value (going right-to-left)",
-      parameters: "4 - (total - initialValue or previously returned value of the function, Value of the current element, array index of the current element, array object the current element belongs to)",
+      parameters: "(1) total - initialValue or previously returned value of the function, (2) value of the current element, (3) array index of the current element (optional), array object the current element belongs to (optional)",
       impactsObject: "No"
    },
    {
       methodName: "map()",
-      objectType: "array",
+      objectType: "Array",
       purpose: "Create a new array with the results of calling a function for every array element.",
-      parameters: "3 - (Value of the current element, array index of the current element, array object the current element belongs to)",
+      parameters: "(1) value of the current element, (2) array index of the current element (optional), (3) array object the current element belongs to (optional)",
       impactsObject: "No"
    },
    {
       methodName: "forEach()",
-      objectType: "array",
+      objectType: "Array",
       purpose: "Call a provided function once for each element in an array, in order.",
-      parameters: "3 - (Value of the current element, array index of the current element, array object the current element belongs to)",
+      parameters: "(1) value of the current element, (2) array index of the current element (optional), array object the current element belongs to (optional)",
       impactsObject: "No"
    },
    // string methods
@@ -139,7 +139,7 @@ const card = [
       methodName: "replace()",
       objectType: "string",
       purpose: "Replace some characters of a string.",
-      parameters: "2 - (value that will be replaced by the new value, value to replace the search value with)",
+      parameters: "(1) value that will be replaced by the new value, (2) value to replace the search value with",
       impactsObject: "No"
    }
    // document methods
